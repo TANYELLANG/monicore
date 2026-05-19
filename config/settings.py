973f73992549a3f3d09add8ca12be371e4806f00
TANYELLANG <geraldntambor@gmail.com>
2026-05-19 11:15:27 +0800

    Add CSRF_TRUSTED_ORIGINS for security
    Added CSRF_TRUSTED_ORIGINS to settings.

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'change-me-in-production')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['monicore.onrender.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://monicore.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
