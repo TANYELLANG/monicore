33362342c572730c18f92afaf06dee226e03a8b1
TANYELLANG <geraldntambor@gmail.com>
2026-05-19 13:40:34 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'change-me-in-production')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['monicore.onrender.com', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://monicore.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -111,3 +110,7 @@ LOGGING = {
         },
     },
 }
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
